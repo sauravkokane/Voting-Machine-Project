64f4d778ff498b928277bcb358eb6c25138319a7
Saurav Kokane <sbkokane7757@gmail.com>
2023-10-09 13:03:19 +0530

    Initial commit

---
+++ src/main/java/com/votingmachine/VotingMachineApplication.java
@@ -0,0 +1,13 @@
+package com.votingmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingMachineApplication.class, args);
+	}
+
+}
